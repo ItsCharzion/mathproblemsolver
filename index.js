1868470bf35c81cfec57a6941de7de46954e90e1
ItsCharzion <106410330+ItsCharzion@users.noreply.github.com>
2023-01-05 22:07:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-let a;
-let b;
-let c;
+let x;
+let y;
+let z;
 
 document.getElementById("submitButton").onclick = function(){
 
-    a = document.getElementById('aTextBox').value;
+    x = document.getElementById('aTextBox').value;
+    x = Number(x);
+
+    y = document.getElementById('bTextBox').value;
+    y = Number(y);
+
+    z = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+
+    document.getElementById('clabel').innerHTML = "Side C: " + z;
+}
+
+let a;
+let b;
+let A;
+let B;
+
+document.getElementById("submitButton1").onclick = function(){
+    A = document.getElementById('aTextBox1').value;
+    A = Number(A);
+    
+    a = document.getElementById('aTextBox2').value;
     a = Number(a);
 
-    b = document.getElementById('bTextBox').value;
-    b = Number(b);
+    B = document.getElementById('aTextBox3').value;
+    B = Number(B);
 
-    c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+    b = (a * (Math.sin(B)))/(Math.sin(A))
 
-    document.getElementById('clabel').innerHTML = "Side C: " + c;
+    document.getElementById('labeli').innerHTML = "Side b: " + b;
 }
